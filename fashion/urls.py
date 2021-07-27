54d27db70263c10340ad7310fcf17a4a99893640
sngina <sngina707@gmail.com>
2021-07-27 10:00:13 +0300

    change folder

--- fashion/urls.py
+++ fashion/urls.py
@@ -3,3 +3,8 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+urlpatterns = [
+    url('^$' , views.get_images , name= 'homepage'),
+    url('^user/' , views.userpage , name= 'username') ,
+    
+]
